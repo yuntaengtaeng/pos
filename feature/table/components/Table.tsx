6c2c37752192d75f9cb02511174caebc7ed8a0f8
yuntaengtaeng <k76253218@gmail.com>
2025-12-25 22:21:44 +0900

    feat: Table 컴포넌트 추가
    - 테이블 정보 표시 컴포넌트 구현
      - 테이블 이름, 주문 목록, 총 가격 표시
      - 최대 3개 주문 항목만 표시하고 나머지는 "외 N개"로 표시
      - OrderItem 타입을 사용하여 주문 정보 표시

---
+++ feature/table/components/Table.tsx
@@ -0,0 +1,92 @@
+import Typo from "@/components/ui/Typo";
+import { Color, Radius, Spacing, Typography } from "@/design-token";
+import { OrderItem } from "@/feature/order/types";
+import React from "react";
+import {
+  Pressable,
+  PressableProps,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from "react-native";
+
+type TableProps = {
+  tableName: string;
+  orders: OrderItem[];
+  style?: ViewStyle;
+} & Omit<PressableProps, "children" | "style">;
+
+const MAX_DISPLAY_LENGTH = 3;
+
+const Table = ({ orders, tableName, style, ...rest }: TableProps) => {
+  const totalPrice = orders.reduce(
+    (sum, order) => sum + order.price * order.count,
+    0
+  );
+
+  const displayedOrders = orders.slice(0, MAX_DISPLAY_LENGTH);
+  const remainingCount = orders.length - MAX_DISPLAY_LENGTH;
+
+  return (
+    <Pressable style={[styles.container, style]} {...rest}>
+      <View style={styles.title}>
+        <Typo variant={Typography.heading.sm}>{tableName}</Typo>
+      </View>
+      <View style={styles.content}>
+        <View>
+          {displayedOrders.map((order) => (
+            <View style={styles.order} key={order.id}>
+              <Typo variant={Typography.body.sm}>{order.name}</Typo>
+              <Typo variant={Typography.body.sm}>{order.count}</Typo>
+            </View>
+          ))}
+        </View>
+        <View style={styles.totalContainer}>
+          {remainingCount > 0 && (
+            <Typo variant={Typography.body.md}>외 {remainingCount}개</Typo>
+          )}
+          <View style={styles.totalPriceWrapper}>
+            <Typo variant={Typography.heading.sm}>
+              {totalPrice.toLocaleString()}원
+            </Typo>
+          </View>
+        </View>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: Radius.md,
+    overflow: "hidden",
+    minHeight: 140,
+  },
+  title: {
+    padding: Spacing.sm,
+    backgroundColor: Color.Neutral.B200,
+  },
+  content: {
+    padding: Spacing.sm,
+    backgroundColor: Color.Neutral.WHITE,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    flex: 1,
+  },
+  order: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  totalContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  totalPriceWrapper: {
+    flex: 1,
+    alignItems: "flex-end",
+  },
+});
+
+export default Table;
